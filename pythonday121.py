c84f8db85e1fbddef607a1fd513b72a6845b96e5
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-28 20:37:11 +0530

    Add files via upload

---
+++ pythonday121.py
@@ -0,0 +1,38 @@
+
+x = "HELL WITH YOU PEOPLE"
+print(x[3:9])
+print(x[6:6])
+print(x[8:-1])
+# negative index
+print(x[-6:-3])
+# modifying the string
+x = "we are creating a new world"
+print(x.lower())
+print(x.upper())
+# splited text
+x = "do it for yourself not for any one"
+print(x.split())
+# replace elements with another one
+x = "so ready for the ressemblence haa"
+print(x.replace("s", "e"))
+x = 'hey'
+y = 'whatsup'
+z = x+y
+print(z)
+# booleans
+print(110 > 11)
+print(10 == 9)
+print(210 < 9)
+print(bool('yoo'))
+print(bool(67))
+# pythonlist
+mylist = (10, 20)
+mylist1 = ("renerya", 'queen alicent')
+print()
+print(mylist)
+mylist = [10, 20, 40, 'mikasa ackerman', 'daemon targaryen']
+print(mylist[2])
+print(mylist[3])
+print(mylist[1])
+print(mylist[4])
+print(len(mylist))

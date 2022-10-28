24f85abbbcce70b8577b13a341d29fd4c13cc152
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-28 20:28:20 +0530

    Add files via upload

---
+++ pythonday12.py
@@ -0,0 +1,38 @@
+
+# regional chunni
+x = "she was just a kid  from brooklyn ,but whole people are going too rude on her beecausw of the character she has but she is good "
+y = "so i was there for a while i dont know if she is ignoring me or not but i dont care a bit , but ik she doesn't need me "
+print(x.upper())
+print(y.lower())
+print(x.lower())
+print(y.upper())
+print(x.replace("so", 'beginning'))
+print(x.split())
+print(y.split())
+print(y.replace("ik", "i know"))
+
+print(type(x))
+print(type(y))
+print(x.replace("e", "o"))
+print(y.replace("r", "u"))
+print(y.replace("k", "p"))
+print(y.replace("h", "k"))
+print(y.replace("l", "r"))
+print(y.replace("y", "t"))
+print(y.replace("a", "q"))
+print(y.replace("t", "c"))
+print(y.replace("d", "k"))
+print(y.replace("k", "t"))
+print(x.split())
+print(x.split())
+print(300 == 56)
+print(562456+62672)
+print(26573-5623)
+print(7627-789065)
+print(7627*789065)
+print('neymar , di maria')
+print("cristano ronaldo, richardo kaka")
+print("richard alexandra andrews", 'godha vaza', 'cheer singh rana',
+      "tp ganesh", 'shshashikal chechi', "i dont know why people have n't the way of possibility")
+print(x)
+# python operators

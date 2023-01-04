06179141dcdf71f5aa7890d5eb10950f73a82870
adharsh.U <114822057+adharsh277@users.noreply.github.com>
2023-01-05 00:07:00 +0530

    Create up.py

---
+++ up.py
@@ -0,0 +1,18 @@
+
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+y = ['unicorn', 'unibrowser']
+
+print('unicorn' in x)
+print("unicorn" in x)
+
+v = ['uoi', 'opi']
+
+print("uoi" in x)
+
+
+x = 5
+
+print(not (x > 3 and x < 10))
